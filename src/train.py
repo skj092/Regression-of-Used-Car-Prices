f672bd7546310c1ae9001c30eafac535edbb98a2
skj092 <sonujha092@gmail.com>
2025-01-15 10:13:59 +0530

    CV Score: 68208.24 | Time: 0.25s

--- src/train.py
+++ src/train.py
@@ -1,14 +1,17 @@
 from pathlib import Path
+import time
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from lightgbm import LGBMRegressor
 from sklearn.metrics import root_mean_squared_error
+from sklearn.preprocessing import LabelEncoder
 
 
 # load dataset
 path = Path('data')
 df = pd.read_csv(path/'train.csv')
+test_df = pd.read_csv(path/'test.csv')
 
 # collect categorical and numerical columns
 cat_cols = df.select_dtypes(include='object').columns.tolist()
@@ -30,6 +33,16 @@ df = fill_missing(df, cat_cols)
 
 # handle categorical columns
 df[cat_cols] = df[cat_cols].astype('category')
+label_encoders = {}
+for col in cat_cols:
+    le = LabelEncoder()
+    df[col] = le.fit_transform(df[col])
+    label_encoders[col] = le
+
+    if col in test_df:
+        test_df[col] = test_df[col].map(
+            lambda s: '<unknown>' if s not in le.classes_ else s)
+
 
 # cross validation
 X = df.drop(['price'], axis=1)
@@ -38,8 +51,10 @@ y = df['price']
 X_train, X_valid, y_train, y_valid = train_test_split(
     X, y, test_size=0.2, random_state=42)
 
+tik = time.monotonic()
 model = LGBMRegressor()
 model.fit(X_train, y_train)
 y_pred = model.predict(X_valid)
 error = root_mean_squared_error(y_pred, y_valid)
-print(f"score is {error}")
+tok = time.monotonic()
+print(f"CV Score : {error:.2f} | Time : {tok-tik:.2f}s")
